9ac07d7e586a771f30b866f2a4c3b530b55f202a
Marora44 <83252659+Marora44@users.noreply.github.com>
2022-03-22 11:18:49 -0400

    moved php to the top

--- loginMember.php
+++ loginMember.php
@@ -1,3 +1,46 @@
+<?php
+    require_once "config.php";
+
+    //variable to not print out that member does not exist if there is information missing
+    $DNE = 0;
+
+    //this section handles when "submit" is clicked on the form
+	if (isset($_POST['sign_in'])) {
+		$username = mysqli_real_escape_string($dbConnect, $_POST['username']);
+		$password = mysqli_real_escape_string($dbConnect, $_POST['password']);
+		
+		if(empty($username)) { array_push($errors, "Enter a username."); $DNE = 1;}
+		if(empty($password)) { array_push($errors, "Enter a password."); $DNE = 1;}
+		
+		$username_exists_query = "SELECT * FROM AccountHolder WHERE username = '$username' LIMIT 1";
+		$result = mysqli_query($dbConnect, $username_exists_query);
+		$member = mysqli_fetch_assoc($result);
+		
+		if(!$member && $DNE==0) {
+				array_push($errors, "Member does not exist.");
+		}
+		else {
+			if(!($member['password'] === $password) && $DNE==0) {
+				array_push($errors, "Password is incorrect.");
+			}
+		}
+		
+		//check to see if we need ot print out errors
+		if(count($errors) == 0) {			
+			
+			//save the member as a session variable
+			$_SESSION['member'] = $member;
+			$id = (int) $member['id'];
+		}
+		else {
+			//display errors
+			foreach($errors as $error) {
+				print($error . "<br>");
+			}
+		}
+	}
+?>
+
 <html>
 	<?php
 		$headerOutput = "<h1>Welcome to the Online Bookstore!</h1>
