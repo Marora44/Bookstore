7462e5567b907a6c8db4091cf5af17200d75c568
kilpy <82245588+kilpy@users.noreply.github.com>
2022-03-28 21:25:14 -0400

    Add files via upload

--- show_orders.php
+++ show_orders.php
@@ -12,10 +12,12 @@ $orderid = 1;
 $bookorderisbn = 0;
 $price = 0;
 $query = "SELECT id, orderDate, quantity, book.isbn, price from bookorder natural join book where userID = $userid";
+$check = 0;
 ?>
 
 <html>
 <h1> Past Orders</h1>
+
 <table>
   <tr>
     <th>ID&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
@@ -26,17 +28,40 @@ $query = "SELECT id, orderDate, quantity, book.isbn, price from bookorder natura
                 $result = mysqli_query($dbConnect, $query);
                 echo "<table>";
                 while ($row = mysqli_fetch_assoc($result)){
-                    $orderid = $row['id'];
-                    $orderdate = $row['orderDate'];
-                    $quantity = $row['quantity'];
-                    $bookorderisbn = $row['isbn'];
-                    #query each book for total price:
-                    $price = ($row['price']) * $quantity;
-                    echo "<tr><td><a href=orderhistory.php>".$orderid."</a>&nbsp</td><td>&nbsp".$orderdate."&nbsp</td><td>&nbsp".$price."</td></tr>";
+                  $orderid = $row['id'];
+                  $quantity = $row['quantity'];
+                  $bookorderisbn = $row['isbn'];
+                  $orderdate = $row['orderDate'];
+                  #if its the same order
+                  if($check == $orderid)
+                  {
+                    $price += (($row['price']) * $quantity);
+                    #echo"".$row['price'];
+                    #echo"&nbsp".$row['quantity'];
+                    #echo $bookorderisbn;
+                  }else #must be a new order
+                  {
+                    #if we definitely have atleast 1 order:
+                    if($check != 0)
+                    {
+                      #dump the TOTAL price before we get the new one
+                      echo "<td>&nbsp".$price."</td></tr><td>";
+                    }
+                    $price = (($row['price']) * $quantity);
+                    echo "<tr><td><a href=orderhistory.php>".$orderid."</a>&nbsp</td><td>&nbsp".$orderdate."&nbsp</td>";
+                    #echo"&nbsp".$row['price'];
+                    #echo"&nbsp".$row['quantity'];
+                    #echo $bookorderisbn;
                   }
+                    #set check to (last) orderid
+                    $check = $orderid;
+                  }
+                  #dump price for the last row, lest it be left out
+                  echo "<td>&nbsp".$price."</td></tr><td>";
                 echo "</table>";
                 ?>
                 
             <?php mysqli_free_result($result);?>
 </table>
+                
 </html>
