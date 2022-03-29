b284d47b9db300981ad672e814fcf188fbaeac16
Marora44 <83252659+Marora44@users.noreply.github.com>
2022-03-29 10:03:49 -0400

    started adding ability to update quantity/delete book in cart

--- shoppingcart.php
+++ shoppingcart.php
@@ -12,6 +12,12 @@ if(isset($_SESSION['id'])){
 }
 else die("something went wrong");
 
+/*  
+    todo:
+    process update/delete
+    add checkout button
+*/
+
 ?>
 
 <html>
@@ -27,27 +33,34 @@ else die("something went wrong");
 
 <body>
     <h1>Your Cart</h1>
-    <table width="70%">
+    <table width="40%">
         <tr>
             <th>ISBN</th>
             <th>Title</th>
             <th>Quantity</th>
             <th>Price</th>
+            <th></th>
 
         </tr>
         <?php
-        $cart = mysqli_query($dbConnect, "SELECT isbn,quantity FROM bookorder WHERE userID = {$userID} AND isPlaced = FALSE");
+        $cart = mysqli_query($dbConnect, "SELECT isbn, quantity FROM bookorder WHERE userID = {$userID} AND isPlaced = FALSE");
         $totalPrice = 0.00;
         while ($cartRow = mysqli_fetch_assoc($cart)) :
-            $bookInfo = mysqli_query($dbConnect, "SELECT title, price FROM book WHERE isbn = \"{$cartRow['isbn']}\"");
+            $bookInfo = mysqli_query($dbConnect, "SELECT title, price, quantity FROM book WHERE isbn = \"{$cartRow['isbn']}\"");
             $bookRow = mysqli_fetch_assoc($bookInfo);
             $totalPrice += $cartRow['quantity'] * $bookRow['price'];
         ?>
             <tr>
                 <td><?= $cartRow['isbn'] ?></td>
                 <td><?= $bookRow['title'] ?></td>
-                <td><?= $cartRow['quantity'] ?></td>
+                <td width="20%"><form style="margin: 5 auto;" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" id="updateQuantity">
+                        <input type="hidden" name="isbn" value="<?= $cartRow['isbn'] ?>" />
+                        <input name="quantity" value="<?= $cartRow['quantity'] ?>" style="width: 4em" type="number" step="1" max="<?= $bookRow['quantity'] + $cartRow['quantity'] ?>">
+                        &nbsp;
+                        <input type="submit" name="update" value="Update">
+                    </form></td>
                 <td>$<?= $cartRow['quantity'] * $bookRow['price'] ?></td>
+                <td><input type="submit" name="del" value="Remove from cart" form="updateQuantity"></td>
             </tr>
         <?php
         
