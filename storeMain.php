b4aa83232cee08b9caab112abdf2eca58ad89ea0
SledTreeSavior <89948594+SledTreeSavior@users.noreply.github.com>
2022-03-30 15:17:19 -0400

    fix

--- storeMain.php
+++ storeMain.php
@@ -55,7 +55,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </style>
         </head>
 
-<<<<<<< HEAD
         <body>
             <h1>All Books</h1>
             <table width="70%">
@@ -96,59 +95,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </table>
         </body>
     </div>
-=======
-<head>
-    <title>Home</title>
-    <style>
-        th {
-            text-align: left;
-        }
-    </style>
-</head>
-
-<body>
-    <?php
-    $headerOutput = "<h1> Welcome to the Online Bookstore!</h1>
-                 <h3><p>All Books</p></h3>";
-    include('header.php');
-    ?>
-    <table width="70%">
-        <tr>
-            <th>ISBN</th>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Price</th>
-            <th></th>
-        </tr>
-        <?php
-        $books = mysqli_query($dbConnect, "SELECT isbn,title,authorID,price,quantity from book");
-        while ($row = mysqli_fetch_assoc($books)) :
-            $instock = $row['quantity'] > 0;
-            $author = mysqli_query($dbConnect, "SELECT id, firstname, lastname FROM author WHERE id = {$row['authorID']}");
-            $authRow = mysqli_fetch_assoc($author);
-            $authorName = $authRow['firstname'] . "&nbsp;" . $authRow['lastname'];
-            mysqli_free_result($author);
-        ?>
-            <tr>
-                <td><?= $row['isbn'] ?></td>
-                <td><?= $row['title'] ?></td>
-                <td><?= $authorName ?></td>
-                <td>$<?= $row['price'] ?></td>
-                <td>
-                    <form style="margin: 5 auto;" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-                        <input type="hidden" name="isbn" value="<?= $row['isbn'] ?>" />
-                        <input name="quantity" style="width: 4em" type="number" step="1" max="<?= $row['quantity'] ?>" <?= $instock ? "value=\"1\"" : "value=\"0\" disabled" ?>>
-                        &nbsp;
-                        <input type="submit" <?= $instock ? "value=\"Add to Cart\"" : "value=\"Out of Stock\" disabled" ?>>
-                    </form>
-                </td>
-            </tr>
-        <?php
-        endwhile;
-        mysqli_free_result($books);
-        ?>
-    </table>
-</body>
-
->>>>>>> df3b8df447374528d247d1efbbc719966caffc1c
 </html>
