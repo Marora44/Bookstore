363ece765c4f8e5c09e30832cf3f79591021f479
kilpy <b.elbridgegarey@gmail.com>
2022-03-30 21:32:18 -0400

    added header

--- storeMain.php
+++ storeMain.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 
+$headerOutput = "<h1>Welcome to the Online Bookstore!</h1>
+<h3><p> Member Login Page:</p></h3>";
+include ('header.php'); 
 require_once "config.php";
 
 $isbn = "";
