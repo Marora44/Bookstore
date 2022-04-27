52233858d85253ee46e001056903b2ece506f6fe
kilpy <b.elbridgegarey@gmail.com>
2022-04-27 19:47:40 -0400

    need to finish recycleview
    implemented adapter for order

--- Phase3/app/Bookstore/app/src/main/java/com/example/bookstore/OrderHistory.java
+++ Phase3/app/Bookstore/app/src/main/java/com/example/bookstore/OrderHistory.java
@@ -1,21 +1,66 @@
 package com.example.bookstore;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 
 public class OrderHistory extends AppCompatActivity {
+    private ArrayList<OrderResults> orderList;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_history);
 
-        String id = getIntent().getStringExtra("id");
+        //String id = getIntent().getStringExtra("id");
+        Intent data = getIntent();
+        try{
+            JSONArray results = new JSONArray(data.getStringExtra("id"));
+            //results = [{"title":"The cat In the Hat","authorID":"1","price":"50.00","author":"Dr Suess"},
+            //           {"title":"Hello World!!","authorID":"1","price":"75.00","author":"Dr Suess"}]
+
+            //Code for displaying results goes here
+            for(int i = 0; i < results.length(); i++){
+
+                //get our object, this is one book's worth of data
+                JSONObject json_data = results.getJSONObject(i);
+
+                //create new BookResults
+                OrderResults resultRow = new OrderResults();
+
+                //set that BookResults' attributes
+                resultRow.id = json_data.getInt("id");
+                resultRow.date = json_data.getString("authorID");
+                resultRow.total = json_data.getDouble("total");
+                //this is our arrayList object, we add our BookResults object to it
+                orderList.add(resultRow);
+            }
+            // will adapt orderList:
+            setAdapter();
 
         }
+        catch (JSONException e){
+            e.printStackTrace();
+        }
     }
+    private void setAdapter(){
+        OrderAdapter adapter = new OrderAdapter(orderList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+    }
+
+}
---
+++ Phase3/app/Bookstore/app/src/main/java/com/example/bookstore/OrderResults.java
@@ -0,0 +1,18 @@
+package com.example.bookstore;
+
+public class OrderResults {
+    public Integer id;
+    public String date;
+    public Double total;
+
+    public Integer getID(){
+        return id;
+    }
+    public String getDate(){
+        return date;
+    }
+    public Double getTotal(){
+        return total;
+    }
+
+}
