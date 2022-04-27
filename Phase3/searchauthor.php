9b1f0a17c63551620260981db535ed176fac6b53
Marora44 <83252659+Marora44@users.noreply.github.com>
2022-04-27 11:50:01 -0400

    Update searchauthor.php

--- Phase3/searchauthor.php
+++ Phase3/searchauthor.php
@@ -4,9 +4,12 @@ $authorResults = array();
 
 $search = isset($_POST['search']) ? $_POST['search'] : "";
 
-$searchResults = mysqli_query($dbConnect, "SELECT title, author, price from Book where author LIKE '%$search%' AND book.isbn != \"become_member\"");
+$searchResults = mysqli_query($dbConnect, "SELECT title, authorID, price FROM book WHERE authorID IN (SELECT id from author where concat(firstname,' ',lastname) LIKE '%$search%') AND book.isbn != \"become_member\"");
 while($row = mysqli_fetch_assoc($searchResults)){
+    $author = mysqli_fetch_assoc(mysqli_query($dbConnect, "SELECT * FROM author WHERE id = {$row['authorID']}"));
+    $row['author'] = $author['firstname'] . " " . $author['lastname'];
     $authorResults[] = $row;
+    
 }
 
 echo json_encode($authorResults);
